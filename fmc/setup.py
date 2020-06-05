a7e9f77bd0635cf837c50ce1d45ba176d8b7083c
x0rzkov <x0rzkov@protonmail.com>
2020-06-05 17:31:52 +0000

    Added fmc crawler

---
+++ fmc/setup.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from setuptools import setup, find_packages
+
+setup(
+    name='fmc',
+    version=datetime.utcnow().date().isoformat(),
+    classifiers=[],
+    keywords='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['memorious', 'datafreeze'],
+    entry_points={
+        'memorious.plugins': [
+            'fmc = fmc:init'
+        ]
+    }
+)
